ebc0d901abe55e0a86510f5515bc2413d8fe2e7b
Robert Sayakhov <roobertdantes@gmail.com>
2022-08-09 22:57:10 +0300

    feat: added button to open menu

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
+
+import { ReactComponent as PlusIcon } from "@assets/images/plus.svg";
 
 import { Header } from "./Header";
 import { Menu } from "./Menu";
@@ -9,11 +11,17 @@ interface Props {
 }
 
 export const Layout: React.FC<Props> = ({ children }) => {
+  const [isMenuVisible, setMenuVisible] = useState(false);
+
   return (
     <Root>
       <Header />
-      <Menu />
+      {isMenuVisible && <Menu />}
       {children}
+      <Footer>
+        {/* TODO: Fix footer block and make it absolute */}
+        <StyledPlusIcon onClick={() => setMenuVisible(true)} />
+      </Footer>
     </Root>
   );
 };
@@ -22,4 +30,19 @@ const Root = styled.div`
   height: 100vh;
   display: flex;
   flex-direction: column;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 15px 50px;
+`;
+
+const StyledPlusIcon = styled(PlusIcon)`
+  cursor: pointer;
+  transition: transform 0.4s ease-in-out;
+
+  :hover {
+    transform: rotate(90deg);
+  }
 `;
